3ac34af73080c2e750bb63a90fba0f562ac6c9e7
Jacob Araiza <jacob.araiza@protonmail.com>
2022-06-07 18:07:54 +0100

    Improving setup.sh

--- script/setup.sh
+++ script/setup.sh
@@ -5,15 +5,15 @@ token_x=$(spl-token create-token | awk '{print $3}' | tr -d '\n')
 token_y=$(spl-token create-token | awk '{print $3}' | tr -d '\n')
 
 # Create users with token accounts
-alice=$(solana-keygen new -o ./alice-keypair.json --force | grep -o "pubkey: .*" | awk '{print $2}' | tr -d '\n')
+alice=$(solana-keygen new -o ./alice-keypair.json --force --no-bip39-passphrase | grep -o "pubkey: .*" | awk '{print $2}' | tr -d '\n')
 alice_x=$(spl-token create-account $token_x --owner $alice | awk '{print $3}')
 #alice_y=$(spl-token create-account $token_y --owner $alice | awk '{print $3}')
-spl-token mint $token_x 100 $alice_x
+spl-token mint $token_x 100 $alice_x > /dev/null
 
-bob=$(solana-keygen new -o ./bob-keypair.json --force | grep -o "pubkey: .*" | awk '{print $2}' | tr -d '\n')
+bob=$(solana-keygen new -o ./bob-keypair.json --force --no-bip39-passphrase | grep -o "pubkey: .*" | awk '{print $2}' | tr -d '\n')
 #bob_x=$(spl-token create-account $token_x --owner $bob | awk '{print $3}')
 bob_y=$(spl-token create-account $token_y --owner $bob | awk '{print $3}')
-spl-token mint $token_y 100 $bob_y
+spl-token mint $token_y 100 $bob_y > /dev/null
 
 echo "Token X: $token_x, Token Y: $token_y"
 echo "Alice $alice with $(spl-token balance --address $alice_x)X in $alice_x"
