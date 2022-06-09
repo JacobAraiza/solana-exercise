bca18679af79cb0ba2fb12919dbda4f0213aa0f0
Jacob Araiza <jacob.araiza@protonmail.com>
2022-06-09 17:29:42 +0100

    Improving run script

--- script/run.sh
+++ script/run.sh
@@ -9,20 +9,26 @@ echo "  Token Y: $token_y"
 
 # Create Alice with 100X
 alice=$(solana-keygen new -o ./alice-keypair.json --force --no-bip39-passphrase | grep -o "pubkey: .*" | awk '{print $2}' | tr -d '\n')
-solana airdrop 1 $alice > /dev/null
+solana airdrop 100 $alice > /dev/null
 alice_x=$(spl-token create-account $token_x --owner $alice | awk '{print $3}')
 spl-token mint $token_x 100 $alice_x > /dev/null
 
 # Create Bob with 100Y
 bob=$(solana-keygen new -o ./bob-keypair.json --force --no-bip39-passphrase | grep -o "pubkey: .*" | awk '{print $2}' | tr -d '\n')
-solana airdrop 1 $bob > /dev/null
+solana airdrop 100 $bob > /dev/null
 bob_y=$(spl-token create-account $token_y --owner $bob | awk '{print $3}')
 spl-token mint $token_y 100 $bob_y > /dev/null
 
 function echo_balances() {
+    alice_x=$(spl-token balance $token_x --owner $alice)
+    alice_y=$(spl-token balance $token_y --owner $alice 2>/dev/null || echo 0)
+    alice_sol=$(solana balance $alice)
+    bob_x=$(spl-token balance $token_x --owner $bob 2>/dev/null || echo 0)
+    bob_y=$(spl-token balance $token_y --owner $bob)
+    bob_sol=$(solana balance $bob)
     echo "Current balances"
-    echo "  Alice $alice $(spl-token balance $token_x --owner $alice)X and $(spl-token balance $token_y --owner $alice 2>/dev/null || echo 0)Y"
-    echo "  Bob $bob $(spl-token balance $token_x --owner $bob 2>/dev/null || echo 0)X and $(spl-token balance $token_y --owner $bob)Y"
+    echo "  Alice $alice ${alice_x}X and ${alice_y}Y $alice_sol"
+    echo "  Bob $bob ${bob_x}X and ${bob_y}Y $bob_sol"
 }
 
 echo_balances
@@ -38,6 +44,7 @@ function post_trade() {
 }
 
 post_trade
+echo_balances
 
 # Cancel trade
 echo "Cancelling trade"
